19f1adae015ff7380dabbd9b3b3620a165be4ce5
kboranbayev <boranbayev.k@gmail.com>
2017-11-21 20:11:53 +0000

    Fixed image intent error

--- controllers/imageSearch.js
+++ controllers/imageSearch.js
@@ -5,9 +5,9 @@ const request = require('request');
 module.exports = (req, res) => {
 	if (req.body.result.action === 'image') {
 		const imageName = req.body.result.parameters['image_name'];
-		const apiUrl = 'https://api.gettyimages.com/v3/search/images?fields=id,title,thumb,referral_destinations&sort_order=best&phrase=' + imageName;
-		console.log('req.body.result.parameters' + JSON.parse(req.body.result.parameters));
-		console.log('searching ' + imageName);
+		//const apiUrl = 'https://api.gettyimages.com/v3/search/images?fields=id,title,thumb,referral_destinations&sort_order=best&phrase=' + imageName;
+		const apiUrl = 'https://api.gettyimages.com/v3/search/images/creative?phrase=' + imageName;
+		//console.log('req.body.result.parameters' + JSON.parse(req.body.result.parameters));
 		request ({
 			uri: apiUrl,
 			methods: 'GET',
@@ -18,7 +18,7 @@ module.exports = (req, res) => {
 			} else {
 				const imageUri = JSON.parse(body).images[0].display_sizes[0].uri;
 				//const imageUri = JSON.parse(body).images[0];
-				//console.log('JSON.parse(body) = ' + JSON.parse(body));
+				console.log('JSON.parse(body) = ' + JSON.parse(body));
 				console.log('imageUri = ' + imageUri);
 				return res.json({
 					speech: imageUri,
